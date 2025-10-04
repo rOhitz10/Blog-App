468822efcee0f1c1fb8963fd57d674ece2ad8677
Rohit Raj Singh <singhrohit1020029@gmail.com>
2025-10-04 21:19:15 +0530

    update vite env file

--- Client/src/Api/axiosConfig.jsx
+++ Client/src/Api/axiosConfig.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
 const api = axios.create({
- baseURL: import.meta.env.API_BASE_URL || 'http://localhost:4000/api/v1',
+  
+ baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api/v1',
  timeout: 15000,
  headers:{
   'content-type' : 'application/json'
  },
+ 
 })
 
 
